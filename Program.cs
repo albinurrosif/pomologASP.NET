ac98f12888e2d10fc7f0b26e8de6c60b7cb1aaa1
Albinur <albi27076@gmail.com>
2026-04-03 00:05:28 +0700

    feat: Add User and Task models, setup DbContext. Fixes #1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Pomolog.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
